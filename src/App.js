d436db3a6d0739e4517a2a367f148386ec99fe1e
kj1ng <kj1ngg@gmail.com>
2023-08-28 17:07:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+import React, { Component } from 'react'
+import "./App.css"
+
+export class App extends Component {
+  
+
+    state  = {
+      fullName: "",
+      bio: "", 
+      imgSrc: "", 
+      profession: "",
+      shows: false 
+     }; 
+    
+  updateState = () =>{
+    this.setState({
+    fullName: "Adekunle Joseph",
+    bio: "Kj is from Kogi State and a digital normad", 
+    imgSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHA3y-nJtVInwwAqyAes75O6EDhquq0LU3xOYPkOyW-g&s", 
+    profession: "Business Analyst",
+    shows: true })
+    
+ }
+
+  // componentDidMount=()=>{
+  //   setInterval(()=>{
+  //  this.SetState(Date.getHours()) 
+  //   }, 2000)
+  
+  // }
+
+  render() {
+    return (
+      <div className = "Profile_Box">
+        <p className = "Profile_Display">
+ <h2>This is my Profile</h2>
+ <h3>FullName: {this.state.fullName}<br/>
+    Bio: {this.state.bio}<br/>
+    Image Source: {this.state.imgSrc}<br/>
+    Profession: {this.state.profession}<br/>
+    
+    <br/>
+ </h3>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        <button onClick={this.updateState}>Click to View my Details </button>
+      </div>
+    )
+  }
 
-export default App;
+}
+export default App
